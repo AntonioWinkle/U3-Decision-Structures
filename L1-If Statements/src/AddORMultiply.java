f564807fdf2e434b0128bef2ed61fee1c08afc4c
winklant000 <winklant000@warren.k12.in.us>
2021-11-12 11:59:48 -0500

    Finished Lesson 1 notes

--- L1-If Statements/src/AddORMultiply.java
+++ L1-If Statements/src/AddORMultiply.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 public class AddORMultiply {
 
     /*
@@ -6,5 +7,29 @@ public class AddORMultiply {
 
     If the first one is larger than the second, mutiply and display the result
     If not, add them and display the results.
+
+    have use input 2 numbers, have methods utilize the input, return the data and show message
      */
+
+    public static void main(String[] args) {
+        double numOne = Double.parseDouble(JOptionPane.showInputDialog("What is the first number?"));
+        double numTwo = Double.parseDouble(JOptionPane.showInputDialog("What is the second number?"));
+
+        if(numOne > numTwo){
+            JOptionPane.showMessageDialog(null,multiply(numOne, numTwo));
+        }
+        if(numOne <= numTwo){
+            JOptionPane.showMessageDialog(null,add(numOne, numTwo));
+        }
+
+    }
+
+
+    public static double add(double numOne, double numTwo){
+        return numOne + numTwo;
+    }
+
+    public static double multiply(double numOne, double numTwo){
+        return numOne * numTwo;
+    }
 }
