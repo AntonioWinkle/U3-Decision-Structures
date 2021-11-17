336a31d556b2e438355056f2a5160d10a26a1bed
winklant000 <winklant000@warren.k12.in.us>
2021-11-17 11:47:17 -0500

    Finished Lesson 2 notes

--- L2-If Else Statements/src/Prom.java
+++ L2-If Else Statements/src/Prom.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Prom {
 
     /* In order to purchase a ticket to go to prom,
@@ -6,4 +8,28 @@ public class Prom {
      */
 
 
+    public static void main(String[] args) {
+
+        int gradeLevel = Integer.parseInt(JOptionPane.showInputDialog("What grade level are you in (9, 10 , 11 , 10: "));
+        int discipline = Integer.parseInt(JOptionPane.showInputDialog("How many suspensions have you had: "));
+        int money = Integer.parseInt(JOptionPane.showInputDialog("About how much money do you have? Round down to the nearest 5$"));
+
+        if(gradeLevel >= 11){
+
+            if(discipline >= 1){
+
+                System.out.println("You can't go to Prom. That's ok, you can't get stood up at Prom either.");
+
+            }else{
+
+                if(money >= 40){
+                    System.out.println("You can get a Prom ticket.");
+                }else{
+                    System.out.println("You can't go to Prom. That's ok, you can't get stood up at Prom either.");
+                }
+            }
+        }else{
+            System.out.println("You can't go to Prom. That's ok, you can't get stood up at Prom either.");
+        }
+    }
 }
